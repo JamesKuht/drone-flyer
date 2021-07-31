c62457755e4743cbdab91bc5dff485b6256f45c1
James Kuht <james.kuht@gmail.com>
2021-07-31 16:01:17 +0100

    next commit

--- drone-first-flight.py
+++ drone-first-flight.py
@@ -1,13 +1,14 @@
 ###### This script is to initialise the drone (once you've connected to it by wifi)
-###### then to turn on it's camera
+###### then to give it a short flight in a square
 
 from djitellopy import Tello
 import cv2
+import time
 
 ##########################################
 width = 640  # width of the image
 height = 480
-startCounter = 1  # if you don't want drone to fly then put 1, if you do, put 0
+startCounter = 0  # if you don't want drone to fly then put 1, if you do, put 0
 ##########################################
 
 # Connect to tello & initialise all of the velocities to zero, for obvious reasons
@@ -34,11 +35,39 @@ while True:
     myFrame = frame_read.frame
     img = cv2.resize(myFrame, (width, height))
 
-    # Show the image
+    # Show the image - still not sure how to get this to stream video whilst flying...
     cv2.imshow("MyResult", img)
-
     # Wait for the q button to stop (of note, you need to have waitKety defined before imshow will work)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         me.land()
         break
 
+    # To go up in the beginning
+    if startCounter == 0:
+        time.sleep(4)
+        me.takeoff()
+        time.sleep(1)
+        me.move_forward(20)
+        time.sleep(1)
+        me.rotate_clockwise(90) # degrees
+        time.sleep(1)
+        me.move_forward(20)
+        time.sleep(1)
+        me.rotate_clockwise(90) # degrees
+        time.sleep(1)
+        me.move_forward(20)
+        time.sleep(1)
+        me.rotate_clockwise(90) # degrees
+        time.sleep(1)
+        me.move_forward(20)
+        time.sleep(1)
+        me.rotate_clockwise(90) # degrees
+        time.sleep(1)
+        me.land()
+        startCounter = 1
+
+
+   
+   
+   
+    
